fabbb4393997966e2895d02895b89388c9a00052
ruk9515 <59643150+ruk9515@users.noreply.github.com>
2020-04-19 15:46:24 +0300

    added docstrings for reader functions

--- utils/readers.js
+++ utils/readers.js
@@ -3,16 +3,33 @@ import { createWorker, setLogging } from "@ffmpeg/ffmpeg";
 
 class Readers {
   /**
+   * Class for reading files and making actions on these data.
+   *
    * @param {Element} subsElement
    * @param {Element} videoElement
    */
 
   constructor(subsElement, videoElement) {
+    /**
+     * Constructor for Readers class.
+     *
+     * Params:
+     *    subsElement (Element): Input file element of the subtitles.
+     *    videoElement (Element): Input file element of the video.
+     */
+
     this.subsElement = subsElement;
     this.videoElement = videoElement;
   }
 
   validate() {
+    /**
+     * Validate both files are uploaded.
+     *
+     * Returns:
+     *    bool: Whether both files are uploaded.
+     */
+
     if (!this.subsElement.files || !this.videoElement.files) {
       return false;
     }
@@ -20,6 +37,12 @@ class Readers {
   }
 
   readSubtitlesAsync() {
+    /**
+     * Reads the subtitles file and resolves the subtitles file content.
+     *
+     * Returns:
+     *    string: The subtitles.
+     */
     return new Promise((resolve, reject) => {
       let reader = new FileReader();
 
@@ -34,6 +57,15 @@ class Readers {
   }
 
   async getData(subtitles_timestamps) {
+    /**
+     * Gets the buffers of the timestamps and formats the return to fit the API.
+     *
+     * Params:
+     *    subtitles_timestamps (Array of Arrays): [[subtitles, start, end], [subtitles, start, end]...]
+     *
+     * Returns:
+     *    Array of Arrays: [[buffer, subtitles], [buffer, subtitles]...]
+     */
     const extension = this.videoElement.files[0].name.split(".")[1];
     const worker = createWorker({
       logger: ({ message }) => console.log(message),
@@ -52,7 +84,7 @@ class Readers {
 
     for (const [subtitles, start, end] of subtitles_timestamps) {
       const buffer = await this.trimVideo(worker, filename, start, end);
-      data.push([this.arrayBufferToBase64(buffer).slice(0, 50), subtitles]);
+      data.push([this.arrayBufferToBase64(buffer), subtitles]);
     }
 
     console.log("Removing...");
@@ -66,6 +98,7 @@ class Readers {
      *
      * Params:
      *      worker (worker): The worker (must be loaded and written into first).
+     *      filename (string): The filename to trim
      *      start (double): Start time in seconds.
      *      end (double): End time in seconds.
      *
@@ -73,14 +106,21 @@ class Readers {
      *       string: The buffer of the file.
      */
 
-    const trimed_filename = "trimed." + filename.split(".")[1];
-    await worker.trim(filename, trimed_filename, 15, 17, "-c copy -y");
+    const trimed_filename = "trimed." + filename.split(".")[1]; // trimed.extension
+    await worker.trim(filename, trimed_filename, start, end, "-c copy -y");
     const { data } = await worker.read(trimed_filename);
     await worker.remove(trimed_filename);
     return data.buffer;
   }
 
   readVideoAsync() {
+    /**
+     * Read the video file and resolve the file content as ArrayBuffer object.
+     *
+     * Returns:
+     *    ArrayBuffer: Binary of the file.
+     */
+
     return new Promise((resolve, reject) => {
       let reader = new FileReader();
 
@@ -96,6 +136,16 @@ class Readers {
   }
 
   arrayBufferToBase64(buffer) {
+    /**
+     * Converts ArrayBuffer object to base64 string.
+     *
+     * Params:
+     *    buffer (ArrayBuffer): The buffer to convert.
+     *
+     * Returns:
+     *    string: base64 string of the buffer.
+     */
+    
     let binary = "";
     const bytes = new Uint8Array(buffer);
     const len = bytes.byteLength;
