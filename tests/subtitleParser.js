b7537c06855787691934d90138d30afcfd7cda96
Eitan <eitan1112@gmail.com>
2020-05-03 18:55:19 +0300

    finished testing for now

--- tests/subtitleParser.js
+++
@@ -1,8 +0,0 @@
-import subtitleParser from '../utils/subtitleParser'
-// const subtitles = require('./samples/subtitles.srt')
-
-// let sp
-
-// beforeEach(() => {
-//     sp = new subtitleParser(subtitles)
-// })
